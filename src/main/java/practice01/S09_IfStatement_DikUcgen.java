2f8099a85ff5c87746299d1682e90c6917c5506c
Gürkay Birinci <117175953+gurkaybirinci@users.noreply.github.com>
2023-05-25 23:46:36 +0300

    25.05.23

---
+++ src/main/java/practice01/S09_IfStatement_DikUcgen.java
@@ -0,0 +1,10 @@
+package practice01;
+
+public class S09_IfStatement_DikUcgen {
+
+    /*
+        Kullanıcıdan üç adet sayı alarak bu sayıların bir dik üçgenin kenar uzunlukları olup olmadığını hesaplayan bir kod yazınız.
+        (Not: Bir üçgenin dik olduğunu anlamak için a^2 + b^2 = c^2 pisagor bağlantısından yararlanabilirsiniz)
+    */
+
+}
